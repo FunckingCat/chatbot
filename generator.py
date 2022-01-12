75205fda9049a056c8db21cbd377425c410947c9
David Zhuravlev <david99111@mial.ru>
2022-01-12 10:19:36 +0300

    functional_changes

--- generator.py
+++ generator.py
@@ -2,8 +2,8 @@ import requests
 from lxml import etree
 import lxml.html
 
-def get_name(sex=10):
-    url = 'https://randomus.ru/name?type=0&sex=' + str(sex) + '&count=1'
+def get_name(sex=10, count = 1):
+    url = 'https://randomus.ru/name?type=0&sex=' + str(sex) + '&count=' + str(count)
     res = requests.get(url)
     tree = lxml.html.document_fromstring(res.text)
     name = tree.xpath('//*[@id="result_tiles"]/div/div/div/div/span/text()')
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from typing import Text
 import telebot
 from telebot import apihelper
 from telebot import types
 from generator import get_name
 
-TOKEN = '828038691:AAHdBswhWnKxBdQbBUy-uT-yXf4sEoD-GH0'
+TOKEN = '5001939735:AAHRF8K892p358JgH5cwApzWh2obwxnG-44'
 bot = telebot.TeleBot(TOKEN)
+
+sex = 10
+format = 0
+ammount = 1
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -15,13 +20,73 @@ def send_welcome(message):
 	markup.add(itembtn1, itembtn2, itembtn3)
 	bot.send_message(message.chat.id, "Выберите пол:", reply_markup=markup)
 
-@bot.message_handler(func=lambda m: True)
+# @bot.message_handler(func=lambda m: m.text == "Мужской" or m.text == "Женский" or m.text == "Оба")
+# def echo_all(message):
+# 	if message.text == "Мужской":
+# 		bot.send_message(message.chat.id, get_name(0))
+# 	elif message.text == "Женский":
+# 		bot.send_message(message.chat.id, get_name(1))
+# 	elif message.text == "Оба":
+# 		bot.send_message(message.chat.id, get_name())
+
+@bot.message_handler(func=lambda m: m.text == "Мужской" or m.text == "Женский" or m.text == "Оба")
 def echo_all(message):
+	global sex
 	if message.text == "Мужской":
-		bot.send_message(message.chat.id, get_name(0))
+		sex = 0
 	elif message.text == "Женский":
-		bot.send_message(message.chat.id, get_name(1))
+		sex = 1
 	elif message.text == "Оба":
-		bot.send_message(message.chat.id, get_name())
+		sex = 10
+	markup = types.ReplyKeyboardMarkup(row_width=2)
+	itembtn1 = types.KeyboardButton('Полное ФИО')
+	itembtn2 = types.KeyboardButton('Фиамилия и инициалы')
+	itembtn3 = types.KeyboardButton('Имя')
+	markup.add(itembtn1, itembtn2, itembtn3)
+	bot.send_message(message.chat.id, "Выберите формат:", reply_markup=markup)
+
+@bot.message_handler(func=lambda m: m.text == "Полное ФИО" or m.text == "Фиамилия и инициалы" or m.text == "Имя")
+def echo_all(message):
+	global format
+	if message.text == "Полное ФИО":
+		format = 0
+	elif message.text == "Фиамилия и инициалы":
+		format = 1
+	elif message.text == "Имя":
+		format = 10
+	markup = types.ReplyKeyboardMarkup(row_width=2)
+	itembtn1 = types.KeyboardButton('5')
+	itembtn2 = types.KeyboardButton('10')
+	itembtn3 = types.KeyboardButton('1')
+	markup.add(itembtn1, itembtn2, itembtn3)
+	bot.send_message(message.chat.id, "Выберите колличество:", reply_markup=markup)
+
+@bot.message_handler(func=lambda m: m.text == "5" or m.text == "10" or m.text == "1")
+def echo_all(message):
+	global ammount
+	if message.text == "1":
+		ammount = 1
+	elif message.text == "5":
+		ammount = 5
+	elif message.text == "10":
+		ammount = 10
+	markup = types.ReplyKeyboardMarkup(row_width=2)
+	itembtn1 = types.KeyboardButton('Мужской')
+	itembtn2 = types.KeyboardButton('Женский')
+	itembtn3 = types.KeyboardButton('Оба')
+	markup.add(itembtn1, itembtn2, itembtn3)
+	bot.send_message(message.chat.id, make_answer(sex, format, ammount), reply_markup=markup)
+
+def make_answer(sex, format, ammount):
+	res = ""
+	for i in range(ammount):
+		name = get_name(sex)
+		if format == 1:
+			splitname = name.split()
+			name = '{} {}. {}.'.format(splitname[0], splitname[1][0], splitname[2][0])
+		elif format == 10:
+			name = name.split()[1]
+		res += name + '\n'
+	return res
 
 bot.infinity_polling()
