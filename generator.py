7177651752921439f035b2018681fb62454073b1
myusername <artur.03834@gmail.com>
2022-01-17 13:40:46 +0300

    another_generanor_update

--- generator.py
+++ generator.py
@@ -1,12 +1,17 @@
 from random import randint
 
-def get_name(race='human', gender='man'):
+def parse(): #просто парсит файл
     file = open('names.txt', encoding='UTF-8')
     read = [''.join(x.split('\n')).split(':') for x in file] # файл парсится на списки в формате [['race-gender'], ['items']]
     file.close()
     keys = [x.pop(0) for x in read] # передаю ключи (формата race-gender) в один список
     items = [x[0].split(', ') for x in read] # передаю значения для ключей в другой список
     names = dict(zip(keys, items)) # объединяю в словарь
+    return names
+
+
+def generate_name(race='human', gender='man'): #генерирует имя
+    names = parse
     first_name = names[race+'-'+gender][randint(0, len(names[race+'-'+gender]))] # из передаваемых функции аргументов race и gender формируются ключи, по которым находятся списки с именами
     last_name = names[race+'-subname'][randint(0, len(names[race+'-subname']))] # и при через randint беруются случайные комбинации
     return (first_name + ' ' + last_name)
