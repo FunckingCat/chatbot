76f388000c0c6365e8304c0a59ca17d7b0d81adb
myusername <artur.03834@gmail.com>
2022-01-21 11:39:12 +0300

    choice_instead_of_randint

--- generator.py
+++ generator.py
@@ -1,12 +1,12 @@
-from random import randint
+from random import choice
 from const import *
 
 def generate_name(list):
 
-    first_name = names[list["race"]+'-'+list["gender"]][randint(0, len(names[list["race"]+'-'+list["gender"]])-1)]
-    last_name = names[list["race"]+'-subname'][randint(0, len(names[list["race"]+'-subname'])-1)]
+    first_name = choice(names[list["race"]+'-'+list["gender"]])
+    last_name = choice(names[list["race"]+'-subname'])
     behavior = list['outlook'] + '-' + list['kindness']
     if list['race'] == 'Дварф' or list['race'] == 'Гном':
-        clan = names[list['race']+'-'+'clan'][randint(0, len(names[list['race']+'-'+'clan'])-1)]
+        clan = choice(names[list['race']+'-'+'clan'])
         return ' '.join([x for x in [behavior, list['class'], first_name, last_name, 'из клана', clan] if x])
     return (' '.join([behavior, list['class'], first_name, last_name]))
